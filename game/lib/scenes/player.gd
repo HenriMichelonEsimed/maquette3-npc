cd5666152581e368f90493d73158f41e23cd201e
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2023-12-23 18:22:28 +0100

    weapon attack speed

--- game/lib/scenes/player.gd
+++ game/lib/scenes/player.gd
@@ -74,9 +74,9 @@ func _physics_process(delta):
 	if (GameState.player_state.hp <= 0): return
 	if Input.is_action_pressed("use") and (not attack_cooldown) and (GameState.current_item != null) and (GameState.current_item is ItemWeapon) and (interactions.node_to_use == null):
 		look_to(get_viewport().get_mouse_position())
-		anim.play(ANIM_ATTACKING, 0.2)
+		anim.play(ANIM_ATTACKING, 0.2, attack_speed_scale)
 		hit_allowed = true
-		timer_use.wait_time = GameMechanics.attack_cooldown(GameState.current_item.speed)
+		timer_use.wait_time = attack_speed_scale
 		move_to_target = null
 		running = false
 		attack_cooldown = true
