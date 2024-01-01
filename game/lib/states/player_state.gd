93ee658e66936356ddde958977352b7db5ee6daa
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-01-01 13:20:32 +0100

    level_0

--- game/lib/states/player_state.gd
+++ game/lib/states/player_state.gd
@@ -1,7 +1,7 @@
 class_name PlayerState extends State
 
 #var zone_name:String = "exteriors/EXT1/level_1"
-var zone_name:String = "start/level_1/level_1"
+var zone_name:String = "level_0/level_0"
 var position:Vector3 = Vector3.ZERO
 var rotation:Vector3 = Vector3.ZERO
 var current_item_type:Item.ItemType = Item.ItemType.ITEM_UNKNOWN
